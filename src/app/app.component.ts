4d97905b0bf20a4359c509d6dacbe2acc2e8dc54
pratikkajale9 <pckajale6@gmail.com>
2020-09-02 21:07:16 +0530

    jj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  myforms = new FormGroup({username : new FormControl() })
 }
